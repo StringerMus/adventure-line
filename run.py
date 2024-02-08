fefbb91b87648c40be8f963bd3d3355174bf3dac
Musaddek Ali <mussali.one@gmail.com>
2024-02-08 16:44:38 +0000

    Add try and except to start_game function  and change to loop in case of error and create select_story function

--- run.py
+++ run.py
@@ -4,11 +4,23 @@
 Introduction = "Welcome to Adventure Line!\n\nYour are in control of what happens in these stories.\n\nThe choices you make will affect the plot but BEWARE, there are dangers and you must decide which choice to make.\nThe wrong decision could end in disaster - or even DEATH."
 print(Introduction)
 
-def start_game(): 
-    start = input("Start Game (y)")
-    if start == "y":
-        story = input("Choose the story:\n\n1.The Night-Shift\n2.The Plane\n3.The Castle")
+def start_game():
+    """
+    Get command to start game from user.
+    User to select the story they want to play.
+    """
+    while True:
+        start = input("Start Game? (y): ")
+        try:
+            if start == "y":
+                break
+        #doesnt work
+        except:
+            print("Please enter 'y' to start game")
 
-
+def select_story():
+    story = input("\nChoose the story:\n\n1.The Night-Shift\n2.The Plane\n3.The Castle\n\n")
+            
 
 start_game()
+select_story()
