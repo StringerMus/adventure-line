7b8c6744e87033402c806ef033d1e54cc1d3ecb0
Musaddek Ali <mussali.one@gmail.com>
2024-02-09 00:30:01 +0000

    Add replay options if story fails

--- run.py
+++ run.py
@@ -19,7 +19,8 @@ def select_story():
     User needs to select which story to play to start a game.
     """
     while True:
-        story = input("\nChoose a story to play:\n\n1.The Castle\n2.The Plane\n3.The Night-Shift\n\nPlease enter (1, 2 or 3):")
+        print("\nChoose a story to play:\n")
+        story = input("\n1.The Castle\n2.The Plane\n3.The Night-Shift\n\nPlease enter (1, 2 or 3):")
         if story == "1":
             the_castle()
             break
@@ -30,7 +31,7 @@ def select_story():
             game_3()
             break
         else:
-            print("\nInvalid key. Please enter the (1/ 2 or 3) to select a story")
+            print("\nInvalid key. Please enter the (1/ 2 or 3) to select a story:")
 
 def the_castle():
     loading = "\nYou have selected 'The Castle'"
@@ -41,24 +42,33 @@ def the_castle():
     Options will be provided to the player
     The player will need to enter a valid key to select a choice and progress the story.
     """
-    #change setting
     castle_set = "\nA Princess has been stolen away by a fire breathing Dragon in its lair as punishment to the King for not paying his yearly tribute.\n\nThe King has promised all the gold the dragon holds to the hero who rescues the Princess and brings her back to him.\n\n You have decided to embark on this bold quest to rescue the Princess and gain riches"
     print(castle_set)
+
+    def replay():
+        retry = "\nWould you like to retry? (y/n):"
+        if retry == "y":
+            the_castle(sit1)
+        elif retry == "n":
+            start_game()
 
     while True:
         sit1 = "\nYou arrive at the Dragons Lair in a shining metal armour, with a sword and shield in your hands. The lair is a big castle inside a mountain, you see the entrance guarded by 2 skeletons with swords\n"
         print(sit1)
-        choice1 = input("\nDo you\n\na.Charge and fight the skeletons or\nb.Run away\n\nEnter here:")
-        if choice1 == "a":
-            a = "You charge at the skeleton guards and they crumble at swing of your mighty sword"
-            print(a)
+        choice = input("\nDo you\n\na.Charge and fight the skeletons or\nb.Run away\n\nEnter here:")
+        if choice == "a":
+            print("You charge at the skeleton guards and they crumble from the hits of your mighty sword.")
             break
-        elif choice1 == "b":
-            b = "You run away cowardly back home with the Princess and gold still sitting in the castle. Mission Failed."
-            print(b)
+        if choice == "b":
+            print("\nYou run away, leaving behind the Princess and the gold to the Dragon. Mission Failed.\n")
+            retry = input("\nWould you like to retry? (y/n):")
             break
-        else:
-            print("\nInvalid key. Please enter the (a or b) to make a choice")
+            if retry == "y":
+                the_castle()
+            elif retry == "n":
+                start_game()
+        #doesnt work - else:
+            #print("\nInvalid key. Please enter the (a or b) to make a choice:")
 
 
 start_game()
